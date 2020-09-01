e6e0a46f5f493fb92e6dac9d3297dc6490a1e266
eric3938 <unmiwi@gmail.com>
2020-09-01 20:14:27 +0800

    data

---
+++ Ko-2/data.js
@@ -0,0 +1,52 @@
+var login = [
+        {
+            addr: "https://www.zhihu.com/#signin", 
+            img: "images/zhihu.jpg", 
+            name: "知乎"
+        },
+        {
+            addr: "https://www.reddit.com/", 
+            img: "images/reddit.jpg", 
+            name: "reddit"
+        },
+        {
+            addr: "https://pan.baidu.com/", 
+            img: "images/dupan.ico", 
+            name: "度盘"
+        },
+        {
+            addr: "https://om.qq.com/userAuth/index", 
+            img: "images/qq-om.jpg", 
+            name: "腾讯·om"
+        },
+        {
+            addr: "http://baijiahao.baidu.com/", 
+            img: "images/baijiahao.jpg", 
+            name: "百家号"
+        },
+        {
+            addr: "https://mp.weixin.qq.com/cgi-bin/loginpage?t=wxm2-login&lang=zh_CN", 
+            img: "images/gongzonghao.jpg", 
+            name: "微信·公众号"
+        },
+        {
+            addr: "https://www.douban.com/people/aitiwei/", 
+            img: "images/douban.jpg", 
+            name: "豆瓣·木馍"
+        },
+        {
+            addr: "https://smartprogram.baidu.com/mappconsole/main/login", 
+            img: "images/baiduxcx.jpg", 
+            name: "百度·小程序"
+        },
+        {
+            addr: "https://www.weiyun.com/disk", 
+            img: "images/weiyun.jpg", 
+            name: "腾讯微云"
+        },
+        {
+            addr: "https://www.weiyun.com/disk", 
+            img: "images/youzack.jpg", 
+            name: "youzack"
+        }
+    ]
